bbdcedc5c95b5baa63b63c42b0a89c3ad00392da
Thomas Hargrove <thargrove@salesforce.com>
2016-08-10 14:07:46 -0700

    Add note to validate script

--- validate.sh
+++ validate.sh
@@ -1,2 +1,5 @@
-#!/bin/bash -xe
+#!/bin/bash
+echo "NOTE: If the next command gives you an error like 'server gave HTTP response to HTTPS client.' then you most likely are missing the insecure registry setting in docker.  See https://git.soma.salesforce.com/sam/sam/wiki/Set-Up-Docker-For-SAM"
+set -xe
 docker run -it --rm -v ${PWD}:/repo/ shared0-samcontrol1-1-prd.eng.sfdc.net:5000/sam-tools /sam/manifest-validator /repo/
+
