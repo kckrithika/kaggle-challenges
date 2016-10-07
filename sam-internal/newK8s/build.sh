460ec99542437de900af02ae2353026996547409
prahlad.joshi <prahlad.joshi@salesforce.com>
2016-10-07 10:51:26 -0700

    Generating json configs instead of yamls

--- sam-internal/newK8s/build.sh
+++ sam-internal/newK8s/build.sh
@@ -14,7 +14,7 @@ if [ ! -f jsonnet/jsonnet ]; then
 fi
 
 #Generates YAML files for a given cluster.
-generateYAMLs() {
+generateConfigs() {
   currentEstate=$1
 
   mkdir -p generated/$currentEstate/appConfigs/yaml
@@ -22,16 +22,10 @@ generateYAMLs() {
 
   for filename in templates/*.jsonnet; do
       appName=$(basename "$filename" .jsonnet)
-      echo "Generating yaml file for $appName in estate $currentEstate"
+      echo "Generating config file for $appName in estate $currentEstate"
       ./jsonnet/jsonnet -V estate=$currentEstate templates/$appName.jsonnet -o generated/$currentEstate/appConfigs/json/$appName.json --jpath .
 
-      #Convert all the files from json to yaml
-      #TODO: Make this as a separate python script 
-      python -c "import sys, yaml, json; print yaml.dump(yaml.load(json.dumps(json.loads(open('generated/$currentEstate/appConfigs/json/$appName.json').read()))), default_flow_style=False)" > generated/$currentEstate/appConfigs/yaml/$appName.yaml
-
   done
-  #Delete json files
-  rm -rf generated/$currentEstate/appConfigs/json
 }
 
 
@@ -41,7 +35,7 @@ declare -a estates=("prd-sam" "prd-samtemp")
 
 for anEstate in "${estates[@]}"
 do
-  generateYAMLs $anEstate
+  generateConfigs $anEstate
 done
 
 # TODO: Add warning when running against out-of-sync git repo
