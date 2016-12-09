1d616f4b33fae596191e4140073f8570d85ed76d
prahlad.joshi <prahlad.joshi@salesforce.com>
2016-12-09 12:21:36 -0800

    Updating SMB version.

--- validate.sh
+++ validate.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "NOTE: If the next command gives you an error like 'server gave HTTP response to HTTPS client.' then you most likely are missing the insecure registry setting in docker.  See https://git.soma.salesforce.com/sam/sam/wiki/Set-Up-Docker-For-SAM"
 
-docker run -it --rm -v ${PWD}:/repo/ shared0-samcontrol1-1-prd.eng.sfdc.net:5000/hypersam:prahlad.joshi-20161117_152828-7e1158d /sam/sam-manifest-builder --root='/repo/' -validateonly
+docker run -it --rm -v ${PWD}:/repo/ shared0-samcontrol1-1-prd.eng.sfdc.net:5000/hypersam:a9e68c1.clean.prahladjos-ltm.20161209_121757 /sam/sam-manifest-builder --root='/repo/' -validateonly
 
 exitcode="$?"
 if [ "0" != "$exitcode" ]
