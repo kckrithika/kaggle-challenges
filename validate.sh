1eae96e42a13fb64995dd5aa9598270dbead42f8
Duncan Ramage Smith <d.smith@salesforce.com>
2017-05-08 12:25:11 -0700

    Update - disallow duplicate functions per namespace

--- validate.sh
+++ validate.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
-SAMTOOLS=ops0-artifactrepo1-0-prd.data.sfdc.net/docker-sam/d.smith/sam-tools:20170505_101751.b42b99ec.clean.duncsmith-ltm
+SAMTOOLS=ops0-artifactrepo1-0-prd.data.sfdc.net/docker-sam/d.smith/sam-tools:20170508_121605.4c962adb.clean.duncsmith-ltm
 
 echo "NOTE: If the docker run command returns a 'BAD_CREDENTIAL' error, you need to run 'docker login ops0-artifactrepo1-0-prd.data.sfdc.net' (one-time). See https://confluence.internal.salesforce.com/x/NRDa (Set up Docker for Sam)"
 
