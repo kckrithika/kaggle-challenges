36d656ae9d4f7c5d870ae248aab29270f436d9bb
Thomas Hargrove <thargrove@salesforce.com>
2017-09-21 10:02:01 -0700

    Add release instructions

--- sam-internal/hypersam.sh
+++ sam-internal/hypersam.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
+# This should always change at the same time as tnrp/pipeline_manifest.json and use the same sha1
+# Follow instructions here for validating the new SMB: https://git.soma.salesforce.com/sam/sam/wiki/Update-SAM-Manifest-Builder#verify-the-new-smb-changes-produce-good-output
+# PR Reviewer: Ensure that PR comments include the results of the above check (image_promotion, control_estate, manifest diffs)
 HYPERSAM=ops0-artifactrepo1-0-prd.data.sfdc.net/tnrp/sam/hypersam:sam-0001268-104afce3
