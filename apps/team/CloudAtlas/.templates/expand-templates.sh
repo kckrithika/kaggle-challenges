b45a07337ecba2385356b859206a31bb446113bf
Drew Shafer <ashafer@salesforce.com>
2018-01-31 11:21:33 -0600

    use templates for proxy too

--- apps/team/CloudAtlas/.templates/expand-templates.sh
+++ apps/team/CloudAtlas/.templates/expand-templates.sh
@@ -1,5 +1,13 @@
+# expand ds template
 for kingdom in dfw phx iad ord frf par; do
         for instance in 1-1 1-2; do
                 m4 -D__MANIFEST_FUNCTION_NAME__=cloudatlas-${kingdom}-dir${instance} < manifest-ds.yaml > ../shared0-samminionatlasdir${instance}-${kingdom}/manifest.yaml
+        done
+done
+
+# expand proxy template
+for kingdom in dfw phx iad ord frf; do
+        for instance in 1-1; do
+                m4 -D__MANIFEST_FUNCTION_NAME__=cloudatlas-${kingdom}-fe${instance} < manifest-proxy.yaml > ../shared0-samminionatlasfe${instance}-${kingdom}/manifest.yaml
         done
 done
