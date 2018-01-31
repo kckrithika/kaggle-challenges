09e4a594b809d36c52692e6e28a343d57e23a877
Drew Shafer <ashafer@salesforce.com>
2018-01-31 14:32:22 -0600

    add paris proxy nodes to template

--- apps/team/CloudAtlas/.templates/expand-templates.sh
+++ apps/team/CloudAtlas/.templates/expand-templates.sh
@@ -6,7 +6,7 @@ for kingdom in dfw phx iad ord frf par; do
 done
 
 # expand proxy template
-for kingdom in dfw phx iad ord frf; do
+for kingdom in dfw phx iad ord frf par; do
         for instance in 1-1 2-1; do
                 m4 -D__MANIFEST_FUNCTION_NAME__=cloudatlas-${kingdom}-fe${instance} < manifest-proxy.yaml > ../shared0-samminionatlasfe${instance}-${kingdom}/manifest.yaml
         done
