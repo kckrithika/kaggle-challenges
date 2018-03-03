c9f589627cae165d4d4e99b3ec60a9d69329edc0
Chanchal Kumar Batra <cbatra@salesforce.com>
2018-03-02 17:02:17 -0800

    Update k4a secret image

--- add-secret.sh
+++ add-secret.sh
@@ -73,7 +73,7 @@ if [[ ! -d "$DIR" ]]; then DIR="$PWD"; fi
 . "$DIR/sam-internal/hypersam.sh"
 
 #Remove next line after the SMB release. This is just to use the updated hypersam image
-HYPERSAM=ops0-artifactrepo1-0-prd.data.sfdc.net/tnrp/sam/hypersam:sam-0001746-68046186
+HYPERSAM=ops0-artifactrepo1-0-prd.data.sfdc.net/tnrp/sam/hypersam:sam-0001755-ecd0a975
 
 docker run \
   --rm \
