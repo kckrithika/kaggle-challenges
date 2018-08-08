875c56de008e60003f9e0fee2efdff4a3114296e
Xiao Zhou <xiao.zhou@salesforce.com>
2018-08-08 16:35:33 -0700

    get rid of extra line

--- sam-internal/hypersam.sh
+++ sam-internal/hypersam.sh
@@ -6,4 +6,3 @@
 # Follow instructions here for validating the new SMB: https://git.soma.salesforce.com/sam/sam/wiki/Update-SAM-Manifest-Builder#verify-the-new-smb-changes-produce-good-output
 # PR Reviewer: Ensure that PR comments include the results of the above check (image_promotion, control_estate, manifest diffs)
 HYPERSAM=ops0-artifactrepo1-0-prd.data.sfdc.net/tnrp/sam/hypersam:sam-0002196-ee698bfb
-
