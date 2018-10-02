98d2cb9a02d383fa26b9b29d630fc2bfd22b5e3d
Vyjayanthi Raja <Vyjayanthi Raja>
2018-10-02 10:55:04 -0700

    upgrade slb manifest builder

--- sam-internal/slbmb.sh
+++ sam-internal/slbmb.sh
@@ -2,4 +2,4 @@
 
 # This should always change at the same time as tnrp/pipeline_manifest.json and use the same sha1
 
-SLBMB=ops0-artifactrepo1-0-prd.data.sfdc.net/tnrp/sdn/slb-manifest-builder:v-0001141-39899f1e
+SLBMB=ops0-artifactrepo1-0-prd.data.sfdc.net/tnrp/sdn/slb-manifest-builder:v-0001233-4877427d
