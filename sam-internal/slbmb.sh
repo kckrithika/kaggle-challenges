e191dd45b1c14180a1f2a6a5370c93f06f66e6bb
Maureen Long <maureen.longsalesforce.com>
2018-11-05 11:29:43 -0800

    increase version of slbmb

--- sam-internal/slbmb.sh
+++ sam-internal/slbmb.sh
@@ -2,4 +2,4 @@
 
 # This should always change at the same time as tnrp/pipeline_manifest.json and use the same sha1
 
-SLBMB=ops0-artifactrepo1-0-prd.data.sfdc.net/tnrp/sdn/slb-manifest-builder:v-0001304-14610d63
+SLBMB=ops0-artifactrepo1-0-prd.data.sfdc.net/tnrp/sdn/slb-manifest-builder:v-0001345-30938bb3
