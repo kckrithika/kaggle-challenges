4b1cbce58613530fc524803294d8346113415e71
Vyjayanthi Raja <vyjayanthi.raja@salesforce.com>
2019-01-31 18:29:51 -0800

    Shellcheck

--- sam-internal/k8s-in/slb/scripts/slb-hosts-updater.sh
+++ sam-internal/k8s-in/slb/scripts/slb-hosts-updater.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# This script adds the given entry to the hosts file
+# Usage: ./slb-hosts-updater.sh <host> <host to override with> <interval>
+# Example usage: ./slb-hosts-updater.sh ops0-artifactrepo2-0-prd.data.sfdc.net ops0-artifactrepo2-0-prd.prd.r.data.sfdc.net 30
+
 set -o errexit
 set -o nounset
 set -o pipefail
@@ -9,16 +13,16 @@ function update_hosts() {
     host=$1
     override=$2
     hosts_file_path="/etc/hosts"
-    ip=$(dig +short ${override} | head -n 1)
+    ip=$(dig +short "${override}" | head -n 1)
     if [ -z "$ip" ]; then
          echo "failed to resolve $override"
-         return 0;
+         return
     fi
 
-    line="$ip\t$host"
-    if [ -n "$(grep ${host} ${hosts_file_path})" ]; then
+    line="$ip $host"
+    if grep -q "${host}" "${hosts_file_path}"; then
          echo "$host is already in hosts file"
-         return 0;
+         return
     fi
 
     echo "Adding $line to $hosts_file_path"
@@ -26,15 +30,14 @@ function update_hosts() {
 }
 
 function update_hosts_loop() {
-    $h=$1
-    $o=$2
     interval_in_seconds=$3
 
     while true
     do
-      update_hosts ${h} ${o}
+      update_hosts "$1" "$2"
+      echo "Sleeping for $interval_in_seconds seconds."
       sleep "$interval_in_seconds"
     done
 }
 
-update_hosts $1 $2 $3
+update_hosts_loop "$1" "$2" "$3"
