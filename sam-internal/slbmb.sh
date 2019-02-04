ce730c79239ff0cb219c7fda41b69cdc2da462ec
Maureen Long <maureen.long@salesforce.com>
2019-02-04 11:18:11 -0800

    update slbmb.sh image to point at the strata-built image instead of the tnrp built image

--- sam-internal/slbmb.sh
+++ sam-internal/slbmb.sh
@@ -2,4 +2,4 @@
 
 # This should always change at the same time as tnrp/pipeline_manifest.json and use the same sha1
 
-SLBMB=ops0-artifactrepo1-0-prd.data.sfdc.net/tnrp/sdn/slb-manifest-builder:v-0001451-a96ca5ad
+SLBMB=ops0-artifactrepo1-0-prd.data.sfdc.net/dva/sdn/slb-manifest-builder:2049-a96ca5ad6608325cf4cfc97bbfab1968f0f7e786
