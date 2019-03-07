b575cfcf340875549daec1059bfc72dd5a139ea5
Maureen Long <maureen.long@salesforce.com>
2019-03-07 15:34:07 -0800

    breakfix slb mb release

--- sam-internal/slbmb.sh
+++ sam-internal/slbmb.sh
@@ -2,4 +2,4 @@
 
 # This should always change at the same time as tnrp/pipeline_manifest.json and use the same sha1
 
-SLBMB=ops0-artifactrepo1-0-prd.data.sfdc.net/dva/slb/slb-manifest-builder:2065-840e1101f78c8e595cd79144f61af3d43a8fb0ae
+SLBMB=ops0-artifactrepo1-0-prd.data.sfdc.net/dva/slb/slb-manifest-builder:2083-4f58886cdd878a3b2b83f711bf92d9a54d5204d0
