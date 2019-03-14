bdcab63965c15eb8535013623de85f7c40b26f37
Shakti <shaktiprakash.das@salesforce.com>
2019-03-14 15:44:32 +0530

    Fix formatting of configmaps

--- sam-internal/k8s-in/service-mesh/generate-istio-templates.sh
+++ sam-internal/k8s-in/service-mesh/generate-istio-templates.sh
@@ -7,6 +7,8 @@ mkdir -p /tmp/istio-upgrade/templates
 cp ${BASH_SOURCE%/*}/istio-ship/rendered.yaml /tmp/istio-upgrade/istio-ship/
 
 # Run tool to convert yaml to jsonnet templates.
+docker pull ops0-artifactrepo1-0-prd.data.sfdc.net/docker-sam/servicemesh/istio-upgrade:dev
+
 docker run \
   -v /tmp/istio-upgrade/istio-ship:/istio-ship \
   -v /tmp/istio-upgrade/templates:/templates \
