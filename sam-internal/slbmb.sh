809672907a669796769f8b24f4d4c14873cad374
PJ Perger <pperger@salesforce.com>
2019-04-24 08:10:56 -0700

    uprev smb to ee945b7

--- sam-internal/slbmb.sh
+++ sam-internal/slbmb.sh
@@ -2,4 +2,4 @@
 
 # This should always change at the same time as tnrp/pipeline_manifest.json and use the same sha1
 
-SLBMB=ops0-artifactrepo1-0-prd.data.sfdc.net/dva/slb/slb-manifest-builder:2122-7efc0c84502265c8bb614d1ac2b5802f5b51b9de
+SLBMB=ops0-artifactrepo1-0-prd.data.sfdc.net/dva/slb/slb-manifest-builder:2128-ee945b7709daf08d513d8d62d272b81b14ade8ae
