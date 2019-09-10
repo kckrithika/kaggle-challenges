9437a320cce6d092aa908ceeab43034c3d446c04
hammondpang <hammondpang@salesforce.com>
2019-09-10 10:24:09 -0700

    Added ability to add a new cluster

--- sam-internal/k8s-in/slb/scripts/public-vip-allocator/slb-public-vip-allocator.py
+++ sam-internal/k8s-in/slb/scripts/public-vip-allocator/slb-public-vip-allocator.py
@@ -24,7 +24,7 @@ def update_reserved_ips(reserved_ips_file_path, cluster, new_ip):
     with open(reserved_ips_file_path, 'r') as jsonnet_file:
         reserved_ip_text = jsonnet_file.read()
 
-    public_reserved_ips_regex = re.compile(r"" + PUBLIC_RESERVED_IPS_FIELD_NAME + ":[^{]*{[^}]*}", re.M)
+    public_reserved_ips_regex = re.compile(r"(" + PUBLIC_RESERVED_IPS_FIELD_NAME + ":[^{]*{[^}]*)}", re.M)
     public_reserved_ips_search_results = public_reserved_ips_regex.search(reserved_ip_text)
     if public_reserved_ips_search_results is None:
         raise Exception('{} was not found'.format(PUBLIC_RESERVED_IPS_FIELD_NAME))
@@ -32,18 +32,20 @@ def update_reserved_ips(reserved_ips_file_path, cluster, new_ip):
     public_reserved_ips_text = public_reserved_ips_search_results.group(0)
     cluster_reserved_ips_regex = re.compile(r'("' + cluster + '": \[[^\]]+)\]')
     cluster_reserved_ips_search_results = cluster_reserved_ips_regex.search(public_reserved_ips_text)
-    if cluster_reserved_ips_search_results is None:
-        raise Exception('{} was not found in {}'.format(cluster, PUBLIC_RESERVED_IPS_FIELD_NAME))
 
-    cluster_reserved_ips_text = cluster_reserved_ips_search_results.group(0)
+    if cluster_reserved_ips_search_results is None:
+        print("Cluster was not found, adding it")
+        cluster_reserved_ips_text = '"' + cluster + '": [\n            ],'
+        public_reserved_ips_text = public_reserved_ips_regex.sub(r'\1    ' + cluster_reserved_ips_text + '\n        }',
+                                                                 public_reserved_ips_text)
+    else:
+        cluster_reserved_ips_text = cluster_reserved_ips_search_results.group(0)
+
     cluster_reserved_ips = ast.literal_eval('{' + cluster_reserved_ips_text + '}')[cluster]
     if new_ip in cluster_reserved_ips:
         print("{} is already in {}".format(new_ip, cluster))
     else:
-        new_public_reserved_ip_text = cluster_reserved_ips_regex.sub(r'\1  "' + new_ip + '",\n             ]',
+        new_public_reserved_ip_text = cluster_reserved_ips_regex.sub(r'\1  "' + new_ip + '",\n            ]',
                                                                      public_reserved_ips_text)
         with open(reserved_ips_file_path, 'w') as jsonnet_file:
-            jsonnet_file.write(reserved_ip_text.replace(public_reserved_ips_text, new_public_reserved_ip_text))
-
-# Input: reserved-ip jsonnet file path, cluster name, new IP
-update_reserved_ips(sys.argv[1], sys.argv[2], sys.argv[3])
+            jsonnet_file.write(public_reserved_ips_regex.sub(new_public_reserved_ip_text, reserved_ip_text))
