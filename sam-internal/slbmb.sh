5ef60cec10c1f12693b4428831ba27e9af11e7f9
hammondpang <hammondpang@salesforce.com>
2019-09-20 10:29:05 -0700

    updated slb-manifest-builder image

--- sam-internal/slbmb.sh
+++ sam-internal/slbmb.sh
@@ -2,4 +2,4 @@
 
 # This should always change at the same time as tnrp/pipeline_manifest.json and use the same sha1
 
-SLBMB=ops0-artifactrepo1-0-prd.data.sfdc.net/dva/slb/slb-manifest-builder:2128-ee945b7709daf08d513d8d62d272b81b14ade8ae
+SLBMB=ops0-artifactrepo1-0-prd.data.sfdc.net/dva/slb/slb-manifest-builder:2223-e4a2929d6ff93ed22cb6e3a3b3155a6a86537b8b
