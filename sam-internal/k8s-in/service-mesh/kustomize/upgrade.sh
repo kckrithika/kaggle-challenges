d3a4c5a0bc1891e6a4dc48085b5e76dd9a9eb1d2
Shakti <shaktiprakash.das@salesforce.com>
2019-11-21 14:21:13 +0530

    Upgrade script enhancements

--- sam-internal/k8s-in/service-mesh/kustomize/upgrade.sh
+++ sam-internal/k8s-in/service-mesh/kustomize/upgrade.sh
@@ -7,6 +7,10 @@ rm -rf tmp/.git
 # Base directory for original helm generated manifests.
 mkdir -p base
 
+# Clear out templates from base if they exist.
+rm -rf base/*/charts
+rm -rf base/*/templates
+
 # Generate istio-init manifests.
 helm template \
 --output-dir base \
@@ -14,31 +18,28 @@ helm template \
 --values tmp/istio-init/sfdc-values/values.yaml  \
 tmp/istio-init
 
-# TODO: Make createServiceAccount configurable at global level, or move it to a separate file sfdc-values/values-falcon.yaml
+## Multiple namespaces support is in helm 3 but we are using helm 2.
+## So, till helm 3, we will run each namespace helm command separately.
 # Generate pilot and sidecarInjectorWebhook manifests.
-# Multiple namespaces support is getting added in helm 3. So, till that time we will run each namespace helm command separately.
 helm template \
 --output-dir base \
 --namespace mesh-control-plane \
 --values tmp/istio/sfdc-values/values.yaml \
 --set gateways.enabled=false \
---set createServiceAccount=true \
---set pilot.createServiceAccount=true \
---set sidecarInjectorWebhook.createServiceAccount=true \
---set gateways.istio-ingressgateway.createServiceAccount=true  \
 tmp/istio
 
-# Generate gateways in core-on-sam-sp2 namespace.
+# Generate gateways manifests in core-on-sam-sp2 namespace.
 helm template \
 --output-dir base \
 --namespace core-on-sam-sp2 \
 --values tmp/istio/sfdc-values/values.yaml \
 --set pilot.enabled=false \
 --set sidecarInjectorWebhook.enabled=false \
---set createServiceAccount=true \
---set gateways.istio-ingressgateway.createServiceAccount=true  \
 --set global.omitSidecarInjectorConfigMap=true \
 tmp/istio
 
 # Delete tmp directory
 rm -rf tmp/
+
+# Replace ship generated yamls with kustomize ones.
+# TODO
