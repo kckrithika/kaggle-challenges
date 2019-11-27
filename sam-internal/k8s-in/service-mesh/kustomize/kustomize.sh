e2b1f14884341df4d9f12cd3cb7f0b5bcb1d65aa
Shakti <shaktiprakash.das@salesforce.com>
2019-11-27 13:10:37 +0530

    Change source for KUSTOMIZE_DIR

--- sam-internal/k8s-in/service-mesh/kustomize/kustomize.sh
+++ sam-internal/k8s-in/service-mesh/kustomize/kustomize.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-KUSTOMIZE_DIR=${KUSTOMIZE_DIR:-${BASH_SOURCE%/*}/kustomize/}
+KUSTOMIZE_DIR=${KUSTOMIZE_DIR:-${BASH_SOURCE%/*}}
 
 # Run kustomize build.
 kustomize build ${KUSTOMIZE_DIR}/overlays/istio-init/ -o ${KUSTOMIZE_DIR}/istio-init.yaml
